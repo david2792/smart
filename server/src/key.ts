27c73251b300764735a2b2c78dd939edae953166
david <david27101992y@gmail.com>
2020-03-03 18:04:32 -0300

    error

--- server/src/key.ts
+++ server/src/key.ts
@@ -3,8 +3,8 @@ export default
     database:
     {
         host: 'localhost',
-        user: 'root',
-        password: '12345',
+        user: 'manager',
+        password: 'developer',
         database: 'smartsystems'
     }
 }
